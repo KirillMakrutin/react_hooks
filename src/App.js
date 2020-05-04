c349a786decbbf965835f8171365029f1c350fc1
Kirill Makrutin <k.makrutin@gmail.com>
2020-05-04 22:47:16 +0300

    useContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import Ingredients from './components/Ingredients/Ingredients';
+import Ingredients from "./components/Ingredients/Ingredients";
+import { AuthContext } from "./components/context/auth-context";
+import Auth from "./components/Auth";
 
-const App = props => {
-  return <Ingredients />;
+const App = (props) => {
+  const authContext = useContext(AuthContext);
+
+  if (authContext.isAuth) {
+    return <Ingredients />;
+  } else {
+    return <Auth />;
+  }
 };
 
 export default App;
---
+++ src/components/context/auth-context.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export const AuthContext = React.createContext({
+  isAuth: false,
+  login: () => {},
+});
+
+export const AuthContextProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const loginHandler = () => {
+    setIsAuthenticated(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{ login: loginHandler, isAuth: isAuthenticated }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
